ccec0145972ea87a62392d76531a87f00d281fcb
roba7o <rhm14148@gmail.com>
2024-03-11 13:15:46 +0000

    test 2

--- script.py
+++ script.py
@@ -1,15 +1,5 @@
-import sys
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-def greet(who_to_greet):
-    greeting = "Hello, {} ".format(who_to_greet)
-    return greeting
-
-print(greet('world'))
-print(greet("bob"))
 
 r = requests.get("https://www.youtube.com/watch?v=06I63_p-2A4&t=108s")
 print(r.status_code)
